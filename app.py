06a6dfcb87cb775fbf90c37df2cf3a77cf117c51
MaxiPruthvi <maxipruthviraj@gmail.com>
2024-10-30 15:47:03 +0530

    cost

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def logout():
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 # Host and port for Render deployment
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# if __name__ == '__main__':
+#     app.run(debug=True)
